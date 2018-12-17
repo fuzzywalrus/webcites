97f9fba5e4d37cb924833fcd9c484a25a11206ed
Greg Gant <me@greggant.com>
2018-12-17 00:20:20 -0800

    Undefined support + date retrieved + return anchor

--- webcite.js
+++ webcite.js
@@ -8,14 +8,15 @@ let citer = {
 	},
 	createArray: () => {
 		const cites = document.querySelectorAll("cite");
-		cites.forEach( (cite) => { //fat arrow make jQuery this goof
+		cites.forEach( (cite) => { 
 			let newObj = {
-				date :  cite.dataset.date,
-				title: cite.dataset.title,
-				source: cite.dataset.source,
+				date :  citer.checkUndefined(cite.dataset.date) ,
+				dateRetrieved:  citer.checkUndefined(cite.dataset.dateretrieved),
+				title: citer.checkUndefined(cite.dataset.title),
+				sourcename:   citer.checkUndefined(cite.dataset.sourcename),
+				source: citer.checkUndefined(cite.dataset.source),
 				i: i
 			}
-			console.log(newObj);
 			let duplicate = citer.checkDuplicates(newObj);
 			if (duplicate === false) { //only push new instance if URL is different
 					i = i + 1; //citations do not start at 0 & only update the index for new URLs
@@ -24,11 +25,23 @@ let citer = {
 			}
 		});
 	},
+	checkUndefined : (cite) => {
+		if (cite === undefined) {
+			return "";
+		} else {
+			return cite;
+		}
+	},
 	writeToDOM: (target) => {
 		let compiledList = "";
 		citeArray.forEach( (instance)  => {
-			const subcite = `<a href="#cite${instance.i}">${instance.i}</a>`;
-			const citation = `<span><a href="${instance.source}" id="cite${instance.i}">${instance.i} ${instance.title}</a> ${instance.date}</span>`
+			
+			const subcite = `<a href="#cite${instance.i}">${instance.i}</a><a name="articleCite${instance.i}"></a>`;
+			
+			if (instance.dateRetrieved !== "" ) {
+				instance.dateRetrieved = "<span class='dateRetrieved'>Date Retreived:</span>" + instance.dateRetrieved;
+			}
+			const citation = `<span class="citationItem"> ${instance.i}<a class="upArrow" href="#articleCite${instance.i}">&#9650;</a>  <a href="${instance.source}" target="_blank" id="cite${instance.i}">${instance.title}</a> ${instance.date} ${instance.dateRetrieved}</span>`
 			compiledList = compiledList + citation;
 			document.querySelectorAll(`[data-source="${instance.source}"]`).forEach( (cite)=> { //queryAll creates an array
 				cite.insertAdjacentHTML('beforeend', subcite);
